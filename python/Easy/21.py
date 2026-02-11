c9d81cbb6c61d2df5c1154f83fd9a99ce9cbf4bf
Howson Luo <1029143329@qq.com>
2026-02-12 00:42:26 +0800

    21: 合并两个有序链表

---
+++ python/Easy/21.py
@@ -0,0 +1,43 @@
+"""
+21. 合并两个有序链表
+
+将两个升序链表合并为一个新的 升序 链表并返回。新链表是通过拼接给定的两个链表的所有节点组成的。 
+"""
+
+# Definition for singly-linked list.
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+
+class Solution:
+    """
+    高手递归
+    https://leetcode.cn/problems/merge-two-sorted-lists/solutions/103891/yi-kan-jiu-hui-yi-xie-jiu-fei-xiang-jie-di-gui-by-/
+    """
+    def mergeTwoLists(self, l1: ListNode, l2: ListNode) -> ListNode:
+        if not l1: return l2  # 终止条件，直到两个链表都空
+        if not l2: return l1
+        if l1.val <= l2.val:  # 递归调用
+            l1.next = self.mergeTwoLists(l1.next,l2)
+            return l1
+        else:
+            l2.next = self.mergeTwoLists(l1,l2.next)
+            return l2
+
+
+if __name__ == '__main__':
+    l1 = ListNode(1)
+    l1.next = ListNode(2)
+    l1.next.next = ListNode(4)
+    l2 = ListNode(1)
+    l2.next = ListNode(3)
+    l2.next.next = ListNode(4)
+    result = Solution().mergeTwoLists(l1, l2)
+
+    while result:
+        print(result.val, end="->")
+        result = result.next
+    
+    # 1->1->2->3->4->4
