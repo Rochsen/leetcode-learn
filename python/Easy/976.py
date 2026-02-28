d22748ba1380df71b648765aa5a0daa775165990
Howson Luo <1029143329@qq.com>
2026-02-28 11:51:12 +0800

    feat: 添加LeetCode 976题解
    实现计算三角形最大周长的算法，先对数组降序排序后遍历检查三角形不等式

---
+++ python/Easy/976.py
@@ -0,0 +1,12 @@
+"""
+976. 三角形的最大周长
+"""
+
+
+class Solution:
+    def largestPerimeter(self, A: List[int]) -> int:
+        A.sort(reverse=True)
+        for i in range(len(A) - 2):
+            if A[i] < A[i + 1] + A[i + 2]:
+                return A[i] + A[i + 1] + A[i + 2]
+        return 0
