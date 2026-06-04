a58f2a08da14c7626fa2caa09c435e4faa3babe7
Howson Luo <1029143329@qq.com>
2026-06-04 13:01:49 +0800

    perf: 优化136.只出现一次的数字的解法-使用位运算+reduce简化代码

--- python/Easy/136.只出现一次的数字.py
+++ python/Easy/136.只出现一次的数字.py
@@ -3,22 +3,12 @@
 #
 # [136] 只出现一次的数字
 #
-
+from functools import reduce
 from typing import List
 
-# @lc code=start
 class Solution:
     def singleNumber(self, nums: List[int]) -> int:
-
-        dict_count = {}
-
-        for n in nums:
-            if n in dict_count:
-                dict_count[n] += 1
-            else:
-                dict_count[n] = 1
-
-        return [k for k,v in dict_count.items() if v == 1][0]
+        return reduce(lambda x, y: x ^ y, nums)
         
 # @lc code=end
 
