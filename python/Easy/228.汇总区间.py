a6187f6fc4a0211b3d798295973f62d984acebe6
Howson Luo <1029143329@qq.com>
2026-06-05 11:26:14 +0800

    perf: 重构228.汇总区间的解法，简化逻辑并提高可读性

--- python/Easy/228.汇总区间.py
+++ python/Easy/228.汇总区间.py
@@ -5,47 +5,26 @@
 #
 from typing import List
 
+
 # @lc code=start
 class Solution:
     def summaryRanges(self, nums: List[int]) -> List[str]:
-        if not nums:
-            return []
+        n = len(nums)
+        res = []
 
-        if len(nums) == 1:
-            return [f"{nums[0]}"]
-
-        # 左区间位置，右区间位置
-        left, right = 0, 0
-        # 全部结果，单个连续区间的结果
-        list_result, tmp_area = [], [f"{nums[left]}", f"{nums[right]}"]
-
-        while right != len(nums) - 1:
-            right += 1
-
-            # 计算区间差值
-            diff = nums[right] - nums[left]
-
-            if diff == 1:
-                tmp_area[-1] = f"{nums[right]}"
-                print(f"tmp_area = {tmp_area}")
-
-                # 遇到最后一个范围时不再进入循环了
-                if right == len(nums) - 1:
-                    list_result.append("->".join(tmp_area))
+        i = 0
+        while i < n:
+            left = nums[i]
+            while i + 1 < n and nums[i + 1] == nums[i] + 1:
+                i += 1
+            right = nums[i]
+            if left == right:
+                res.append(str(left))
             else:
-                if tmp_area[0] == tmp_area[-1]:
-                    tmp_area.pop()
+                res.append(f"{left}->{right}")
+            i += 1
+        return res
 
-                list_result.append("->".join(tmp_area))
-                tmp_area = [f"{nums[right]}", f"{nums[right]}"]
-
-                # 遇到最后一个范围时不再进入循环了
-                if right == len(nums) - 1:
-                    list_result.append(f"{nums[right]}")
-
-            left += 1
-
-        return list_result
 
 # @lc code=end
 
