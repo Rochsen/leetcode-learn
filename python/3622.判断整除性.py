e6932c671dc7773801e0382296529948ab860b63
Howson Luo <1029143329@qq.com>
2026-08-22 09:28:05 +0800

    feat(Solution): 添加整除性判断功能
    新增了检查数字是否能被其各位数字之和与乘积之和整除的功能。
    当输入数字小于10时直接返回False，否则计算各位数字的和与乘积的总和，
    然后判断原数字是否能被该总和整除。

--- python/3622.判断整除性.py
+++ python/3622.判断整除性.py
@@ -5,8 +5,21 @@
 #
 
 # @lc code=start
+from math import prod
+
+
 class Solution:
     def checkDivisibility(self, n: int) -> bool:
+        if n < 10:
+            return False
+        
+        # 1. 获取数字列表
+        list_digits = [int(i) for i in str(n)]
+
+        # 2. 获取数字列表的 summary
+        summary = sum(list_digits) + prod(list_digits)
+
+        return n % summary == 0
         
 # @lc code=end
 
